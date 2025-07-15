af9780791e57948647da90432dbc75d88951cb60
Katelyn <katelyn.lai@outlook.com>
2025-07-15 14:58:04 +0100

    adding more dependencies

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const fruitsApp = express();
+const cors = require('cors');
 const fruitsRoutes = require("./routes/fruits")
 const nutritionRoutes = require("./routes/nutritionRoutes")
 
 fruitsApp.get('/', (req, res) => {
    res.send('Hello Fruity!')
 })
+
+fruitsApp.use(cors());
 fruitsApp.use(express.json());
 fruitsApp.use('/fruits', fruitsRoutes)
 fruitsApp.use('/nutrition', nutritionRoutes)
